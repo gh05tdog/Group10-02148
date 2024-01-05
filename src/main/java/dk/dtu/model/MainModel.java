d55c64cef76177646b7547d7e567771b55de1de4
Oliver Hansen <s224754@dtu.dk>
2024-01-05 11:01:56 +0100

    Added file strutre

---
+++ src/main/java/dk/dtu/model/MainModel.java
@@ -0,0 +1,4 @@
+package dk.dtu.model;
+
+public class MainModel {
+}
--- src/main/java/dk/dtu/view/JavaFXLoader.java
+++ src/main/java/dk/dtu/view/JavaFXLoader.java
@@ -1,18 +1,18 @@
-package dk.dtu;
+package dk.dtu.view;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.jspace.FormalField;
 import org.jspace.SequentialSpace;
 import org.jspace.Space;
 
-import javafx.application.Application;
-import javafx.event.*;
-import javafx.scene.control.Button;
-import javafx.scene.input.*;
-import javafx.scene.layout.StackPane;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class JavaFX_Test extends Application {
+public class JavaFXLoader extends Application {
 
     public static void main(String[] args) throws InterruptedException {
         Space inbox = new SequentialSpace();
