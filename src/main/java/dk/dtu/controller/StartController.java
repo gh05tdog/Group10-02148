c1f941905ff4a563eb31192ce2c0239e1d2bb07a
Oliver Hansen <s224754@dtu.dk>
2024-01-09 11:09:06 +0100

    Trying to reworking the sever

--- src/main/java/dk/dtu/controller/StartController.java
+++ src/main/java/dk/dtu/controller/StartController.java
@@ -1,7 +1,5 @@
-
 package dk.dtu.controller;
 
-import dk.dtu.App;
 import dk.dtu.Server;
 import dk.dtu.config;
 import dk.dtu.model.AppModel;
@@ -15,8 +13,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.jspace.ActualField;
-import org.jspace.FormalField;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -27,71 +23,63 @@ public class StartController {
     public Button CreateLobby;
     public Button JoinGameIP;
     public TextField IpField;
+    private AppModel model;
+    private Server server;
 
     public StartController() throws IOException {
-        AppModel model = new AppModel();
-    }
 
+    }
 
     @FXML
     public void JoinLobbyAction(ActionEvent event) {
-        try {
-
-            // Load the new FXML file
-            Parent newRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/dk/dtu/view/PopUpID.fxml")));
+        try{
+            Parent newRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/dk/dtu/view/PopUpIP.fxml")));
             Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             currentStage.setOnCloseRequest(e -> Platform.exit());
             currentStage.setScene(new Scene(newRoot));
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
     }
 
     @FXML
     private void CreateLobbyAction(ActionEvent event) {
         try {
-            Server server = new Server();
-            server.startServer();
+            server = new Server(); // Instantiate the Server here
+            server.startServer();  // Start the server
             loadChatUI(event);
         } catch (Exception e) {
-            System.out.println("Error: " + e);
+            System.out.println("Error creating lobby: " + e);
         }
     }
 
-    public void JoinGameBasedOnIP(ActionEvent actionEvent) {
-        try {
-            config.setIp(IpField.getText());
-            //Try to connect to the server
-            AppModel model = new AppModel();
-            try {
-                model.setServer(config.getIp());
-                model.getServer().get(new ActualField("server"), new FormalField(String.class));
-            } catch (Exception e) {
-                System.out.println("Error: " + e);
-                PopUpController.showPopUp("Could not connect to the server");
-                return;
-            }
-
-            model.killServer();
-            loadChatUI(actionEvent);
-        } catch (Exception e) {
-            System.out.println("error: " + e);
-        }
-    }
-
-    private void loadChatUI(ActionEvent event) throws Exception {
+    private void loadChatUI(ActionEvent event) throws IOException {
         Parent newRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/dk/dtu/view/App_view.fxml")));
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         currentStage.setOnCloseRequest(e -> Platform.exit());
         currentStage.setScene(new Scene(newRoot));
     }
 
-    public void openStartScreen() throws Exception {
+    public void openStartScreen() throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/dk/dtu/view/StartScreen.fxml")));
         Stage stage = new Stage();
         stage.setTitle("MoonLit Noir");
         stage.setScene(new Scene(root));
         stage.setOnCloseRequest(e -> Platform.exit());
         stage.show();
+    }
+
+
+    public void JoinGameBasedOnIP(ActionEvent actionEvent) {
+        try {
+            //Set ip
+            String ip = IpField.getText();
+            config.setIp(ip);
+            loadChatUI(actionEvent);
+        } catch (Exception e) {
+            System.out.println("Error joining lobby: " + e);
+        }
     }
 }
