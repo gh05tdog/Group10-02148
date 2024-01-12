cafab093e9bbfb8e8aa24a235abf5b7ff1378a7e
s214930 <s214930@student.dtu.dk>
2024-01-12 15:15:13 +0100

    Added "Snitch" powers to StatusControl

--- src/test/java/dk/dtu/StatusControlTest.java
+++ src/test/java/dk/dtu/StatusControlTest.java
@@ -4,59 +4,131 @@ import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatusControlTest {
     private StatusControl statusControl;
 
+    private ScheduledExecutorService executorService;
+
     @BeforeEach
     void setUp() throws InterruptedException {
         int noOfPlayers = 5;
-        String[] rolelist = {"Villager", "Seer", "Werewolf", "Villager", "Werewolf"};
+        String[] rolelist = {"Citizen", "Snitch", "Bodyguard", "Mafia", "Mafia"};
 
         statusControl = new StatusControl(noOfPlayers, rolelist);
+
+        executorService = Executors.newScheduledThreadPool(1);
     }
 
+    /**
+    An unprotected player should be killed when attemptMurder is called
+     */
     @Test
     void testMurderWithoutProtection() throws InterruptedException {
-
-        statusControl.attemptMurder(1);
-
         assertFalse(statusControl.conductor[1].isSecured());
+        statusControl.attemptMurder(1);
         assertTrue(statusControl.conductor[1].isKilled());
     }
 
+    /**
+     A protected player should stay alive when attemptMurder is called
+     */
     @Test
     void testMurderWithProtection() throws InterruptedException {
-
+        assertFalse(statusControl.conductor[2].isSecured());
         statusControl.protectPlayer(2);
+        assertTrue(statusControl.conductor[2].isSecured());
 
-        assertFalse(statusControl.conductor[2].isKilled());
-
-        // Wait for 5 seconds
         Thread.sleep(5000);
 
         assertTrue(statusControl.conductor[2].isSecured());
-
         statusControl.attemptMurder(2);
-
         assertFalse(statusControl.conductor[2].isKilled());
     }
 
+    /**
+     If the protection experies, the player becomes unprotected and can be killed again
+     */
     @Test
     void testMurderAfterProtectionExpires() throws InterruptedException {
-
-        statusControl.protectPlayer(3);
-
-        assertFalse(statusControl.conductor[3].isKilled());
-
-        // Wait for 11 seconds
-        Thread.sleep(11000);
-
         assertFalse(statusControl.conductor[3].isSecured());
+        statusControl.protectPlayer(3);
+        assertTrue(statusControl.conductor[3].isSecured());
 
-        statusControl.attemptMurder(3);
+        executorService.schedule(() -> {
+            assertFalse(statusControl.conductor[3].isSecured());
+            try {
+                statusControl.attemptMurder(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            assertTrue(statusControl.conductor[3].isKilled());
+        }, 11, TimeUnit.SECONDS);
 
-        assertTrue(statusControl.conductor[3].isKilled());
     }
+
+    /**
+     The duration of which a player is protected should not be extended
+     if .protectPlayer is called whilst already protected
+     */
+    @Test
+    void testDoubleProtectionNotPossible() throws InterruptedException {
+        assertFalse(statusControl.conductor[4].isSecured());
+        statusControl.protectPlayer(4);
+        assertTrue(statusControl.conductor[4].isSecured());
+
+        Thread.sleep(5000);
+
+        statusControl.protectPlayer(4);
+        assertTrue(statusControl.conductor[4].isSecured());
+
+        Thread.sleep(6000);
+
+        assertFalse(statusControl.conductor[4].isSecured());
+    }
+
+    /**
+     Multiple people can be protected at once
+     */
+    @Test
+    void testMultipleProtectedPeople() throws InterruptedException {
+        assertFalse(statusControl.conductor[1].isSecured());
+        assertFalse(statusControl.conductor[2].isSecured());
+        assertFalse(statusControl.conductor[2].isSecured());
+        statusControl.protectPlayer(1);
+        statusControl.protectPlayer(2);
+        statusControl.protectPlayer(3);
+        assertTrue(statusControl.conductor[1].isSecured());
+        assertTrue(statusControl.conductor[2].isSecured());
+        assertTrue(statusControl.conductor[3].isSecured());
+    }
+
+    /**
+     If nobody is inside a player's house, their role can be revealed
+     */
+    @Test
+    void testSnitchOnUnprotectedPlayer() throws InterruptedException {
+        assertTrue(statusControl.houses.lookInsideHouse(4));
+        //assertEquals(statusControl.getPlayerRole(4),"Mafia");
+        assertEquals(statusControl.getPlayerRole(4),statusControl.conductor[4].getRole());
+    }
+
+    /**
+     If someone is protected or someone is inside the house, their role will not be revealed
+     */
+    @Test
+    void testSnitchOnProtectedPlayer() throws InterruptedException {
+        statusControl.protectPlayer(4);
+        assertFalse(statusControl.houses.lookInsideHouse(4));
+        assertNotEquals(statusControl.getPlayerRole(4),statusControl.conductor[4].getRole());
+    }
+
+
+
 }
